0be8328ba3e72ebdfaad2702003fd42bf83a072b
Bipin <biping579@gmail.com>
2023-02-26 21:09:13 +0530

    backend bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -95,6 +95,7 @@ exports.loginController = async (req, res) => {
         first_name: user.first_name,
         last_name: user.last_name,
         email: user.email,
+        role: user.role
       },
       token,
     });
